59d4ac39398cb69926809bb7ce76423efe6c991b
tangxiangong <tangxiangong@gmail.com>
2025-06-20 19:46:41 +0800

    新增图像处理功能，包含加载图像和将图像转换为张量的函数，支持可选的图像缩放。

--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,28 @@
+use crate::error::{AppError, AppResult};
+use candle_core::{DType, Device, Tensor};
+use image::DynamicImage;
 
+pub fn load_image(path: impl AsRef<std::path::Path>) -> AppResult<DynamicImage> {
+    image::ImageReader::open(&path)
+        .map_err(|e| AppError::bad_request(e.to_string()))?
+        .decode()
+        .map_err(|e| AppError::bad_request(e.to_string()))
+}
+
+pub fn image_to_tensor(
+    path: impl AsRef<std::path::Path>,
+    resize_shape: Option<(u32, u32)>,
+) -> AppResult<Tensor> {
+    let original_img = load_image(&path)?;
+    let img = match resize_shape {
+        Some((width, height)) => {
+            original_img.resize_to_fill(width, height, image::imageops::FilterType::Triangle)
+        }
+        None => original_img,
+    };
+    let width = img.width() as usize;
+    let height = img.height() as usize;
+    let raw_data = img.into_rgb8().into_raw();
+    let data = Tensor::from_vec(raw_data, (width, height, 3), &Device::Cpu)?.permute((2, 0, 1))?;
+    Ok((data.to_dtype(DType::F32)? / 255.0)?)
+}
