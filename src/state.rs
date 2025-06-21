cdc4c78fa421ccd5122e4de2ac05d0f9cdb1ec32
tangxiangong <tangxiangong@gmail.com>
2025-06-21 10:57:31 +0800

    新增get函数，加载配置并初始化App实例，处理设备选择及错误日志记录。

--- src/state.rs
+++ src/state.rs
@@ -1,1 +1,41 @@
+use crate::configration::Config;
+use search_images_core::{
+    App,
+    config::{Device, MobilenetConfig},
+    error::Error,
+};
 
+pub fn get() -> App {
+    let config = Config::load();
+    let device = match config.mobilenet.device.into_device() {
+        Ok(_) => config.mobilenet.device,
+        Err(e) => {
+            tracing::warn!("Failed to use device: {:?}, using CPU instead", e);
+            Device::Cpu
+        }
+    };
+    let mobilenet_config = MobilenetConfig {
+        kind: config.mobilenet.kind,
+        device,
+    };
+    match App::new(&config.db, &mobilenet_config) {
+        Ok(app) => app,
+        Err(e) => {
+            match e {
+                Error::CUDAError => tracing::error!("Failed to use CUDA"),
+                Error::MetalError => tracing::error!("Failed to use Metal"),
+                Error::HuggingFaceApiError(e) => {
+                    tracing::error!("Failed to use HuggingFace API: {}", e)
+                }
+                Error::QdrantBuildError(e) => {
+                    tracing::error!("Failed to build Qdrant: {}", e)
+                }
+                Error::CandleError(e) => {
+                    tracing::error!("Failed to use Candle: {}", e)
+                }
+                _ => tracing::error!("Failed to load app: {:?}", e),
+            }
+            std::process::exit(1);
+        }
+    }
+}
