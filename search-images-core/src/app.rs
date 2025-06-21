2a4c226a50dcbb33e78f13fefd3b98dea8a05e5e
tangxiangong <tangxiangong@gmail.com>
2025-06-21 17:13:20 +0800

    优化 `App` 结构体的初始化逻辑，新增异步支持和集合存在性检查，确保在创建新集合时的错误处理更加健壮。

--- search-images-core/src/app.rs
+++ search-images-core/src/app.rs
@@ -1,9 +1,13 @@
 use crate::{
     config::{DbConfig, MobilenetConfig},
     error::{Error, Result},
-    extractor::Extractor,
+    extractor::{Extractor, FEATURE_SIZE},
 };
-use qdrant_client::{Qdrant, QdrantBuilder, config::CompressionEncoding};
+use qdrant_client::{
+    Qdrant, QdrantBuilder,
+    config::CompressionEncoding,
+    qdrant::{CreateCollectionBuilder, Distance, VectorParamsBuilder},
+};
 
 pub struct App {
     db: Qdrant,
@@ -12,7 +16,7 @@ pub struct App {
 }
 
 impl App {
-    pub fn new(db_config: &DbConfig, mobilenet_config: &MobilenetConfig) -> Result<Self> {
+    pub async fn new(db_config: &DbConfig, mobilenet_config: &MobilenetConfig) -> Result<Self> {
         let device = mobilenet_config.device().into_device()?;
         let extractor = Extractor::new(mobilenet_config.kind(), &device)?;
         let qdrant_url = format!("http://{}:{}", db_config.url(), db_config.port());
@@ -24,10 +28,24 @@ impl App {
             .build()
             .map_err(|e| Error::QdrantBuildError(e.to_string()))?;
 
+        let collection = db_config.collection().to_string();
+
+        if !db
+            .collection_exists(&collection)
+            .await
+            .map_err(|e| Error::CollectionError(e.to_string()))?
+        {
+            db.create_collection(CreateCollectionBuilder::new(&collection).vectors_config(
+                VectorParamsBuilder::new(FEATURE_SIZE as u64, Distance::Cosine),
+            ))
+            .await
+            .map_err(|e| Error::CollectionError(e.to_string()))?;
+        }
+
         Ok(Self {
             db,
             extractor,
-            collection: db_config.collection().to_string(),
+            collection,
         })
     }
 
