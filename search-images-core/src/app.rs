3e060394171acecb80e57062151c0cd7501910b1
tangxiangong <tangxiangong@gmail.com>
2025-06-21 16:55:53 +0800

    在 `App` 结构体中新增 `collection` 字段及相关方法，优化了数据库配置的初始化逻辑，提升了代码的可读性和可维护性。

--- search-images-core/src/app.rs
+++ search-images-core/src/app.rs
@@ -8,13 +8,14 @@ use qdrant_client::{Qdrant, QdrantBuilder, config::CompressionEncoding};
 pub struct App {
     db: Qdrant,
     extractor: Extractor,
+    collection: String,
 }
 
 impl App {
     pub fn new(db_config: &DbConfig, mobilenet_config: &MobilenetConfig) -> Result<Self> {
-        let device = mobilenet_config.device.into_device()?;
-        let extractor = Extractor::new(mobilenet_config.kind, &device)?;
-        let qdrant_url = format!("http://{}:{}", &db_config.url, db_config.port);
+        let device = mobilenet_config.device().into_device()?;
+        let extractor = Extractor::new(mobilenet_config.kind(), &device)?;
+        let qdrant_url = format!("http://{}:{}", db_config.url(), db_config.port());
         let db = QdrantBuilder::from_url(&qdrant_url)
             .connect_timeout(std::time::Duration::from_secs(30))
             .timeout(std::time::Duration::from_secs(30))
@@ -23,7 +24,11 @@ impl App {
             .build()
             .map_err(|e| Error::QdrantBuildError(e.to_string()))?;
 
-        Ok(Self { db, extractor })
+        Ok(Self {
+            db,
+            extractor,
+            collection: db_config.collection().to_string(),
+        })
     }
 
     pub fn db(&self) -> &Qdrant {
@@ -32,5 +37,9 @@ impl App {
 
     pub fn extractor(&self) -> &Extractor {
         &self.extractor
+    }
+
+    pub fn collection(&self) -> &str {
+        &self.collection
     }
 }
