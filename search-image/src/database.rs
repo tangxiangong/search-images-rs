a12b5ba88fecbd374469c0da3471c2dce82c4945
tangxiangong <tangxiangong@gmail.com>
2025-06-23 11:47:16 +0800

    重构数据库操作，新增批量插入、删除和搜索功能，优化错误处理，增强数据库交互的灵活性和可用性。

--- search-image/src/database.rs
+++ search-image/src/database.rs
@@ -1,53 +1,41 @@
 use crate::error::{Error, Result};
 use qdrant_client::{
     Payload, Qdrant,
-    qdrant::{PointStruct, UpsertPointsBuilder},
+    qdrant::{
+        Condition, DeletePointsBuilder, Filter, GetPointsBuilder, PointStruct, PointsIdsList,
+        QueryPointsBuilder, RetrievedPoint, ScoredPoint, UpsertPointsBuilder, r#match::MatchValue,
+    },
 };
 use serde::Serialize;
+use std::collections::HashMap;
 use uuid::Uuid;
-
-pub async fn add_one<T: Serialize>(
-    client: &Qdrant,
-    collection: &str,
-    data: &[f32],
-    payload: T,
-) -> Result<()> {
-    let point = create_point(data, payload)?;
-
-    upsert_points(client, collection, vec![point]).await?;
-    Ok(())
-}
 
 pub async fn add<T: Serialize>(
     client: &Qdrant,
     collection: &str,
     data: &[Vec<f32>],
     payload: &[T],
-) -> Result<()> {
+) -> Result<Vec<String>> {
     if data.len() != payload.len() {
         return Err(Error::UpsertPointsError(
             "data and payload must have the same length".to_string(),
         ));
     }
+    let ids = (0..data.len())
+        .map(|_| Uuid::new_v4().to_string())
+        .collect::<Vec<_>>();
     let points = data
         .iter()
-        .zip(payload.iter())
-        .map(|(data, payload)| create_point(data, payload))
+        .zip(payload.iter().zip(ids.iter()))
+        .map(|(data, (payload, id))| {
+            let json_val = serde_json::to_value(payload)?;
+            let payload = Payload::try_from(json_val)
+                .map_err(|e| Error::JsonToPayloadError(e.to_string()))?;
+            let point = PointStruct::new(id.to_string(), data.to_vec(), payload);
+            Ok(point)
+        })
         .collect::<Result<Vec<_>>>()?;
 
-    upsert_points(client, collection, points).await?;
-    Ok(())
-}
-
-fn create_point<T: Serialize>(data: &[f32], payload: T) -> Result<PointStruct> {
-    let json_val = serde_json::to_value(payload)?;
-    let payload =
-        Payload::try_from(json_val).map_err(|e| Error::JsonToPayloadError(e.to_string()))?;
-    let point = PointStruct::new(Uuid::new_v4().to_string(), data.to_vec(), payload);
-    Ok(point)
-}
-
-async fn upsert_points(client: &Qdrant, collection: &str, points: Vec<PointStruct>) -> Result<()> {
     let res = client
         .upsert_points(UpsertPointsBuilder::new(collection, points).wait(true))
         .await
@@ -56,9 +44,87 @@ async fn upsert_points(client: &Qdrant, collection: &str, points: Vec<PointStruc
         return Err(Error::UpsertPointsError("upsert points failed".to_string()));
     }
 
+    Ok(ids)
+}
+
+pub async fn delete_by_ids(client: &Qdrant, collection: &str, ids: &[String]) -> Result<()> {
+    let point_ids = ids.iter().map(|id| id.clone().into()).collect::<Vec<_>>();
+    let res = client
+        .delete_points(
+            DeletePointsBuilder::new(collection)
+                .points(PointsIdsList { ids: point_ids })
+                .wait(true),
+        )
+        .await
+        .map_err(|e| Error::DeletePointsError(e.to_string()))?;
+    if res.result.is_none() {
+        return Err(Error::DeletePointsError("delete points failed".to_string()));
+    }
+
     Ok(())
 }
 
-pub async fn delete(_client: &Qdrant, _collection: &str, _id: &str) -> Result<()> {
-    todo!()
+pub async fn delete_by_payloads<T: Into<MatchValue>>(
+    client: &Qdrant,
+    collection: &str,
+    payloads: HashMap<String, T>,
+) -> Result<()> {
+    let response = client
+        .delete_points(
+            DeletePointsBuilder::new(collection)
+                .points(Filter::must(
+                    payloads
+                        .into_iter()
+                        .map(|(key, value)| Condition::matches(&key, value.into()))
+                        .collect::<Vec<_>>(),
+                ))
+                .wait(true),
+        )
+        .await
+        .map_err(|e| Error::DeletePointsError(e.to_string()))?;
+    if response.result.is_none() {
+        return Err(Error::DeletePointsError("delete points failed".to_string()));
+    }
+    Ok(())
+}
+
+pub async fn search_by_ids(
+    client: &Qdrant,
+    collection: &str,
+    ids: &[String],
+    with_payload: bool,
+    with_vectors: bool,
+) -> Result<Vec<RetrievedPoint>> {
+    let point_ids = ids.iter().map(|id| id.clone().into()).collect::<Vec<_>>();
+
+    let response = client
+        .get_points(
+            GetPointsBuilder::new(collection, point_ids)
+                .with_payload(with_payload)
+                .with_vectors(with_vectors),
+        )
+        .await
+        .map_err(|e| Error::SearchPointsError(e.to_string()))?;
+    Ok(response.result)
+}
+
+pub async fn similarity_search(
+    client: &Qdrant,
+    collection: &str,
+    feature: &[f32],
+    k: usize,
+    with_payload: bool,
+    with_vectors: bool,
+) -> Result<Vec<ScoredPoint>> {
+    let response = client
+        .query(
+            QueryPointsBuilder::new(collection)
+                .query(feature.to_vec())
+                .limit(k as u64)
+                .with_payload(with_payload)
+                .with_vectors(with_vectors),
+        )
+        .await
+        .map_err(|e| Error::SearchPointsError(e.to_string()))?;
+    Ok(response.result)
 }
