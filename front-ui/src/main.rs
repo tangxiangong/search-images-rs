a0f9d6f91f11f95810223891d694a2346e95d612
tangxiangong <tangxiangong@gmail.com>
2025-06-23 11:47:43 +0800

    重构前端计数器组件，新增静态和动态列表功能，优化按钮样式和交互逻辑，提升用户体验。

--- front-ui/src/main.rs
+++ front-ui/src/main.rs
@@ -1,26 +1,87 @@
-use leptos::{
-    ev,
-    html::{button, div, span},
-    prelude::*,
-};
+use std::marker::PhantomData;
+
+use leptos::{logging, prelude::*};
 
 fn main() {
-    mount_to_body(|| counter(0));
+    mount_to_body(Counter);
 }
 
-fn counter(initial_value: i32) -> impl IntoView {
-    let (count, set_count) = signal(initial_value);
+#[component]
+fn Counter() -> impl IntoView {
+    let (count, set_count) = signal(0);
 
-    div().child((
-        button()
-            .on(ev::click, move |_| set_count.set(0))
-            .child("Clear"),
-        button()
-            .on(ev::click, move |_| *set_count.write() -= 1)
-            .child("-1"),
-        span().child(("Value: ", count, "!")),
-        button()
-            .on(ev::click, move |_| *set_count.write() += 1)
-            .child("+1"),
-    ))
+    view! {
+        <h1>"Counter"</h1>
+        <div>
+            <button
+                on:click=move |_| set_count.update(|n| *n += 1)
+                class:red=move || count.get() % 2 == 0
+                style="position: absolute"
+                style:left=move || format!("{}px", count.get() + 100)
+                style:background-color=move || format!("rgb({}, {}, 100)", count.get(), 100)
+                style:max-width="400px"
+                style=("--columns", move || count.get().to_string())
+            >
+                "Click me: "
+                {count}
+            </button>
+            <progress max="50" value=count />
+            <SizeOf<String> />
+            <StaticList />
+        </div>
+        <div>
+            <DynamicList />
+        </div>
+    }
+}
+
+#[component]
+fn SizeOf<T: Sized>(#[prop(optional)] _ty: PhantomData<T>) -> impl IntoView {
+    std::mem::size_of::<T>()
+}
+
+#[component]
+fn StaticList() -> impl IntoView {
+    let item = (0..10).map(|i| view! { <li>{i}</li> }).collect_view();
+
+    view! { <ul>{item}</ul> }
+}
+
+#[derive(Debug, Clone)]
+struct DatabaseEntry {
+    key: String,
+    value: i32,
+}
+
+#[component]
+fn DynamicList() -> impl IntoView {
+    let (data, set_data) = signal(vec![
+        DatabaseEntry {
+            key: "foo".to_string(),
+            value: 0,
+        },
+        DatabaseEntry {
+            key: "bar".to_string(),
+            value: 1,
+        },
+        DatabaseEntry {
+            key: "baz".to_string(),
+            value: 2,
+        },
+    ]);
+
+    view! {
+        <button on:click=move |_| {
+            set_data
+                .update(|data| {
+                    for entry in data {
+                        entry.value *= 2;
+                    }
+                });
+            logging::log!("{:?}", data.get())
+        }>"Update"</button>
+        <For each=move || data.get() key=|state| state.key.clone() let(child)>
+            <p>{child.value}</p>
+        </For>
+    }
 }
